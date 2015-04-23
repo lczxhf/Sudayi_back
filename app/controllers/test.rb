68375ad309f4e0d3397ac1ee3c6a06e5fdad27f9
lczxhf <lczxhf@sina.com>
2015-04-23 18:49:50 +0800

    0423

--- app/controllers/test.rb
+++ app/controllers/test.rb
@@ -1,7 +1,7 @@
 SudayiBack::App.controllers :test do
   
 get :login do
-  render :login
+  render :login,:layout=>false
 end
 get :home do
   @account=SupplierAccount.find(params[:id])
@@ -18,5 +18,29 @@ end
 get :employee do
   @account=SupplierAccount.find(params[:id])
   render :employee
+end
+get :product_upload do
+  @account=SupplierAccount.find(params[:id])
+  render :product_upload
+end
+get :product_tag do
+  @account=SupplierAccount.find(params[:id])
+  render :product_tag
+end
+get :now_order do
+  @account=SupplierAccount.find(params[:id])
+  render :now_order
+end
+get  :complete_order do
+  @account=SupplierAccount.find(params[:id])
+  render :complete_order
+end
+get :cancel_order do
+  @account=SupplierAccount.find(params[:id])
+  render :cancel_order
+end
+get :back_goods do
+  @account=SupplierAccount.find(params[:id])
+  render :back_goods
 end
 end
